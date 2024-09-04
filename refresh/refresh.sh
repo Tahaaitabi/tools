990bb2b2695c7c49ccbea54921170c7492babcd5
archTaha <aitabitaha@gmail.com>
2024-09-04 14:28:26 +0200

    Readme

---
+++ refresh/refresh.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+read -p "Choose a port: " port
+
+function open {
+  if ls "index."* &> /dev/null; then
+    python3 -m http.server "$port" &
+    server_pid=$!
+    sleep 1
+    firefox --new-window "http://localhost:$port" &
+    browser_pid=$!
+    sleep 2  # Give some time for the browser to open
+    ff_clear_cache
+    init_check
+    hash_check
+  else
+    echo "No index detected. Please create one and try again."
+  fi
+}
+
+function ff_clear_cache {
+  title_tag=$(grep -m 1 -i '<title>' ./index.* | awk -F '[</>]' '{print $3}')
+  clear_cache="ctrl+shift+r"
+  xdotool search --name "$title_tag" key "$clear_cache"
+}
+
+function init_check {
+  mkdir -p /tmp/refresh/
+  ls -lahR > /tmp/refresh/contents
+  md5sum /tmp/refresh/contents > /tmp/refresh/digest
+}
+
+function hash_check {
+  while true; do 
+    sleep 1  # Add a delay between checks to prevent high CPU usage
+    ls -lahR > /tmp/refresh/contents 
+    new_hash=$(md5sum /tmp/refresh/contents)
+    old_hash=$(cat /tmp/refresh/digest)
+
+    if [ "$new_hash" != "$old_hash" ]; then 
+      ff_clear_cache
+      init_check  # Update the hash after clearing the cache
+    fi
+  done
+}
+
+open
+
